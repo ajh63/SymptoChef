4e81c88934fdee8e889c10e1b782b1d49becc183
Ashley Jisue Hong <ahpuppy63@gmail.com>
2023-07-31 11:37:04 -1000

    Initial Commit

---
+++ SymptoChef/SymptoChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymptoChefApp.swift
+//  SymptoChef
+//
+//  Created by Ashley Hong on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct SymptoChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
