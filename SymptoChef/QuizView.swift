f0d01f1706e407bff038918d47a35a81c2eccaac
Yavnil <ayavnia@gmail.com>
2023-08-02 12:43:16 -0700

    Committing quiz view/page

---
+++ SymptoChef/QuizView.swift
@@ -0,0 +1,80 @@
+//
+//  QuizView.swift
+//  SymptoChef
+//
+//  Created by Sam Yavnilovitch on 8/2/23.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    @State var soreThroat : Bool
+    @State var fever : Bool
+    @State var stomachAche : Bool
+    @State var nausea : Bool
+    var body: some View {
+        VStack {
+            Text("SymptoQuiz\n")
+                .font(.largeTitle)
+            Text("What are your current symptoms?\n")
+                .font(.title2)
+            Text("Check all that are applicable")
+                .foregroundColor(.gray)
+            Spacer()
+            VStack(alignment: .leading) {
+                Toggle(isOn : $soreThroat) {
+                    HStack {
+                        Spacer()
+                        Text("Sore throat")
+                        Spacer()
+                    }
+                }
+                .toggleStyle(CheckboxToggleStyle())
+                .padding()
+                Toggle(isOn : $fever) {
+                    HStack {
+                        Spacer()
+                        Text("Fever")
+                        Spacer()
+                    }
+                }
+                .toggleStyle(CheckboxToggleStyle())
+                .padding()
+                Toggle(isOn : $stomachAche) {
+                    HStack {
+                        Spacer()
+                        Text("Stomachache")
+                        Spacer()
+                    }
+                }
+                .toggleStyle(CheckboxToggleStyle())
+                .padding()
+                Toggle(isOn : $nausea) {
+                    HStack {
+                        Spacer()
+                        Text("Nausea")
+                        Spacer()
+                    }
+                }
+                .toggleStyle(CheckboxToggleStyle())
+                .padding()
+            }
+            Spacer()
+            NavigationLink(destination : ResultsView()) {
+                Text("Submit")
+            }
+            .buttonBorderShape(.roundedRectangle)
+            .buttonStyle(.bordered)
+            .foregroundColor(.black)
+            .backgroundStyle(.blue)
+            .padding()
+        }
+        .padding()
+    }
+}
+
+struct QuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuizView(soreThroat : false, fever : false, stomachAche : false, nausea : false)
+    }
+}
---
+++ SymptoChef/ResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsView.swift
+//  SymptoChef
+//
+//  Created by Sam Yavnilovitch on 8/2/23.
+//
+
+import SwiftUI
+
+struct ResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultsView()
+    }
+}
