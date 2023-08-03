3382db87f201bf0c7d69d1dca0fd7ebfcf6709e9
Ashley Jisue Hong <ahpuppy63@gmail.com>
2023-08-03 09:12:08 -1000

    finished help page and meet the developers page (besides font)

--- SymptoChef/AboutView.swift
+++ SymptoChef/AboutView.swift
@@ -1,40 +1,49 @@
 //
-//  AboutView.swift
-//  SymptoChef
+//  MeetTheDevelopers.swift
+//  symptochef side
 //
-//  Created by Al Yavnilovitch on 8/3/23.
+//  Created by Ashley Hong on 2023/08/03.
 //
 
 import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color.backgroundPink
-                    .ignoresSafeArea()
-            }
-            .toolbar {
-                ToolbarItemGroup(placement: .bottomBar) {
-                    NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)) {
-                        Text("Home")
-                            .foregroundColor(.textPurple)
-                    }
-                    Spacer()
-                    NavigationLink(destination: QuizView(soreThroat: false, fever: false, headache: false, nausea: false, heartburn: false)) {
-                        Text("Quiz")
-                            .foregroundColor(.textPurple)
-                    }
-                    Spacer()
-                    NavigationLink(destination: HelpPage()) {
-                        Text("Help")
-                            .foregroundColor(.textPurple)
-                    }
-                    Spacer()
-                    NavigationLink(destination: ResearchView()) {
-                        Text("Research")
-                            .foregroundColor(.textPurple)
-                    }
+        ZStack{
+            Color.backgroundPink.edgesIgnoringSafeArea(.all)
+            ScrollView{
+                VStack{
+                    Text("Meet The Developers")
+                        .font(.title)
+                        .fontWeight(.heavy).foregroundColor(.textPurple)
+                        .padding(.top)
+                    //Ashley Jisue
+                    HStack{
+                        Image("ajPic").resizable().aspectRatio(contentMode: .fit).frame(height: 250).cornerRadius(70)
+                        VStack{
+                            Text("Ashley Jisue Hong").font(.headline).fontWeight(.heavy).foregroundColor(.textPurple).padding(.trailing, 21.0).padding(.bottom, -1.0)
+                            Text("Nice to meet you! My name is Ashley Jisue Hong and I'm a rising highschool junior from Honolulu, HI. My favorite food to eat when sick is kimchi and chicken jook (Korean rice porridge).").font(.body).foregroundColor(.textPurple).padding(.horizontal, 3.0)
+                        }
+                    }.padding()
+                    
+                    //Jamie
+                    HStack{
+                        Image("jamiePic").resizable().aspectRatio(contentMode: .fit).frame(height: 250).cornerRadius(70)
+                        VStack{
+                            Text("Jamie Scanlan").font(.title3).fontWeight(.bold).foregroundColor(.textPurple).padding(.trailing, 21.0).padding(.bottom, -1.0)
+                            Text("Hi! My name is Jamie and I'm a rising senior in highschool from San Francisco, CA. My favorite food to eat when sick is a Korean rice porridge called jook.").font(.body).foregroundColor(.textPurple).padding(.horizontal, 3.0)
+                        }
+                    }.padding()
+                    
+                    //Sam
+                    HStack{
+                        Image("samPic").resizable().aspectRatio(contentMode: .fit).frame(height: 250).cornerRadius(70)
+                        VStack{
+                            Text("Sam Yavnilovitch").font(.title3).fontWeight(.bold).foregroundColor(.textPurple).padding(.trailing, 21.0).padding(.bottom, -1.0)
+                            Text("Hi, I'm Sam! I am so happy you've decided to visit our SymptoChef application, and I hope you've enjoyed your stay. My favorite food to eat when sick is probably the vegetable soup my mom makes!").font(.body).foregroundColor(.textPurple).padding(.horizontal, 3.0)
+                        }
+                    }.padding()
+                    
                 }
             }
         }
--- SymptoChef/HelpPage.swift
+++ SymptoChef/HelpPage.swift
@@ -11,33 +11,35 @@ struct HelpPage: View {
     var body: some View {
         ZStack {
             Color.backgroundPink.edgesIgnoringSafeArea(.all)
-            VStack {
-                Text("Help")
-                    .font(.largeTitle)
-                    .fontWeight(.bold).padding(.bottom).foregroundColor(.textPurple)
-                Text("This page describes each feature on our app and helps the user navigate.")
-                    .font(.headline)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal).padding(.bottom).foregroundColor(.textPurple)
-                Text("About")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .padding(.bottom, 1.0).foregroundColor(.textPurple)
-                Text("As children, it’s a common experience for our parents to feed us various home recipes and remedies to make us feel better when sick, without really telling us what’s in them.")
-                    .multilineTextAlignment(.leading)
-                    .padding(.horizontal).padding(.bottom).foregroundColor(.textPurple)
-                Text("Our app aims to provide easy and flavorful recipes dependent on the user’s inputted sickness symptoms (cough, sore throat, etc) so that users can easily search for and recreate these remedies.")
-                    .padding(.horizontal).foregroundColor(.textPurple)
-                Text("Home Page Navigation")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .padding(.top).foregroundColor(.textPurple)
-                Image("helpdescription")
-                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-                
-                
+            ScrollView{
+                VStack{
+                    VStack {
+                        Text("Help")
+                            .font(.largeTitle)
+                            .fontWeight(.bold).padding(.vertical).foregroundColor(.textPurple)
+                        Text("This page describes each feature on our app and helps the user navigate.")
+                            .font(.headline)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal).padding(.bottom).foregroundColor(.textPurple)
+                        Text("About")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .padding(.bottom, 1.0).foregroundColor(.textPurple)
+                        Text("As children, it’s a common experience for our parents to feed us various home recipes and remedies to make us feel better when sick, without really telling us what’s in them.")
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal).padding(.bottom).foregroundColor(.textPurple)
+                        Text("Our app aims to provide easy and flavorful recipes dependent on the user’s inputted sickness symptoms (cough, sore throat, etc) so that users can easily search for and recreate these remedies.")
+                            .padding(.horizontal).foregroundColor(.textPurple)
+                    }
+                    .padding()
+                    Text("Home Page Navigation")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.top).foregroundColor(.textPurple)
+                    Image("helpdescription")
+                        .resizable().aspectRatio(contentMode: .fill)
+                }
             }
-            .padding()
         }
     }
 }
