a9ed17c5437ddb644bb8fccf8f08a71e4894c562
Ashley Jisue Hong <ahpuppy63@gmail.com>
2023-08-03 06:22:26 -1000

    help page edit and added custom colors

--- SymptoChef/HelpPage.swift
+++ SymptoChef/HelpPage.swift
@@ -1,28 +1,49 @@
 //
-//  HelpPage.swift
-//  SymptoChef
+//  ContentView.swift
+//  symptochef side
 //
-//  Created by Ashley Hong on 2023/08/01.
+//  Created by Ashley Hong on 2023/08/02.
 //
 
 import SwiftUI
 
 struct HelpPage: View {
     var body: some View {
-        VStack{
-            Text("Help")
-                .font(.title)
-            Text("About This App")
-                .padding(4.0)
-            Text("As children, it’s a common experience for our parents to feed us various home recipes and liquid remedies to make us feel better, without really telling us what’s in them. Our app aims to provide easy and flavorful recipes dependent on the user’s inputted sickness symptoms (cough, sore throat, etc) so that users can easily search for and recreate these remedies. We chose this idea because we all love food and wanted to create something that could be useful for users at home, especially young adults living on their own or anyone else who has to take care of themselves when sick.")
-                .padding(.leading, 3.0)
+        ZStack {
+            Color.backgroundPink.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Help")
+                    .font(.largeTitle)
+                    .fontWeight(.bold).padding(.bottom).foregroundColor(.textPurple)
+                Text("This page describes each feature on our app and helps the user navigate.")
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal).padding(.bottom).foregroundColor(.textPurple)
+                Text("About")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 1.0).foregroundColor(.textPurple)
+                Text("As children, it’s a common experience for our parents to feed us various home recipes and remedies to make us feel better when sick, without really telling us what’s in them.")
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal).padding(.bottom).foregroundColor(.textPurple)
+                Text("Our app aims to provide easy and flavorful recipes dependent on the user’s inputted sickness symptoms (cough, sore throat, etc) so that users can easily search for and recreate these remedies.")
+                    .padding(.horizontal).foregroundColor(.textPurple)
+                Text("Home Page Navigation")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.top).foregroundColor(.textPurple)
+                Image("helpdescription")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
                 
+                
+            }
+            .padding()
         }
     }
 }
 
 struct HelpPage_Previews: PreviewProvider {
     static var previews: some View {
-        HelpPage()
+        ContentView()
     }
 }
---
+++ SymptoChef/colors.swift
@@ -0,0 +1,15 @@
+//
+//  colors.swift
+//  SymptoChef
+//
+//  Created by Ashley Hong on 2023/08/03.
+//
+
+import SwiftUI
+
+extension Color{
+    public static let backgroundPink: Color = Color(UIColor(red: 240/255, green: 230/255, blue: 239/255, alpha:1.0))
+    public static let textPurple: Color = Color(UIColor(red: 156/255, green: 137/255, blue: 184/255, alpha:1.0))
+    public static let submitBlue: Color = Color(UIColor(red: 100/255, green: 213/255, blue: 249/255, alpha:1.0))
+    
+}
